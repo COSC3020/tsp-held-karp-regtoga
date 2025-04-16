3e1d6d24eb8dcee0eebfb01acd3b13d1f3e770d1
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-04-16 15:07:47 -0600

    Yay its dynamic!!!

--- code.js
+++ code.js
@@ -5,9 +5,9 @@ function tsp_hk(distance_matrix) {
     }
 
     var minTour = Infinity;
-    //start at every starting location
     for (var i = 0; i < cities.length; i++) {
-        var cost = heldKarp(cities, i, distance_matrix);
+        var memo = {};
+        var cost = heldKarp(cities, i, distance_matrix, memo);
         if (cost < minTour) {
             minTour = cost;
         }
@@ -20,35 +20,35 @@ function tsp_hk(distance_matrix) {
 }
 
 
-function heldKarp(cities, start, distance_matrix){
-    //cities is an array [0,1,2,3,4] that describes the unvisited cities
-    //start is a index in the array of cities that describes where we are in the distance matrix
-    //The distance matrix provides us with the weighted paths required to do the problem
+function heldKarp(cities, start, distance_matrix, memo){
+    var key = cities + start;
+    if (memo[key] != undefined) {
+        return memo[key];
+    }
 
     if (cities.length == 2){
-        //return length of tour that starts at start, goes directly to other city in cities
+        let result;
         if (cities[0] == start){
-            return distance_matrix[start][cities[1]];
-        }else{
-            return distance_matrix[start][cities[0]];
+            result = distance_matrix[start][cities[1]];
+        } else {
+            result = distance_matrix[start][cities[0]];
         }
-        
-    }else{
-        //return the minimum of
-        //for each city in cities, unless the city is start
-            // reduce the set of cities that are unvisited by one  (the old start), set the new start, add on the distance from old start to new start
-            //heldKarp(cities - start, city) + distance from start to city
+        memo[key] = result;
+        return result;
+    } else {
         var holderofpaths = [];
         var city_minus_start = cities.filter(element => element !== start);
 
         for (var i = 0; i < cities.length; i++){
             if (cities[i] != start){
                 var distance_from_start_to_city = distance_matrix[start][cities[i]];
-                holderofpaths.push(heldKarp(city_minus_start, cities[i], distance_matrix) + distance_from_start_to_city);
+                holderofpaths.push(
+                    heldKarp(city_minus_start, cities[i], distance_matrix, memo) + distance_from_start_to_city
+                );
             }
-            
         }
 
-        return Math.min(...holderofpaths);
+        memo[key] = Math.min(...holderofpaths);
+        return memo[key];
     }
 }
